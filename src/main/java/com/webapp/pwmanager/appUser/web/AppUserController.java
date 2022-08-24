6dca697a1629f052cd550ba0313e6e7f23ec275a
Dmartins2695 <62670499+Dmartins2695@users.noreply.github.com>
2022-08-24 21:18:52 +0100

    imports otimization

--- src/main/java/com/webapp/pwmanager/appUser/web/AppUserController.java
+++ src/main/java/com/webapp/pwmanager/appUser/web/AppUserController.java
@@ -1,18 +1,11 @@
 package com.webapp.pwmanager.appUser.web;
 
-import com.webapp.pwmanager.appUser.repository.AppUserRepository;
-import com.webapp.pwmanager.appUser.domain.AppUser;
 import com.webapp.pwmanager.appUser.service.AppUserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/user")
