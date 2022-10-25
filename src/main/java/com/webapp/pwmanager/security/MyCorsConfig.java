af6c817152b2ff52da852902c3a3328af86dfb0b
Dmartins2695 <62670499+Dmartins2695@users.noreply.github.com>
2022-10-25 23:58:20 +0100

    fix: cross origin fix

--- src/main/java/com/webapp/pwmanager/security/MyCorsConfig.java
+++ src/main/java/com/webapp/pwmanager/security/MyCorsConfig.java
@@ -17,11 +17,12 @@ public class MyCorsConfig implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         final HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, enctype");
-        response.setHeader("Access-Control-Max-Age", "3600");
+        final HttpServletRequest request = (HttpServletRequest) servletRequest;
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
         if (HttpMethod.OPTIONS.name().equalsIgnoreCase(((HttpServletRequest) servletRequest).getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
