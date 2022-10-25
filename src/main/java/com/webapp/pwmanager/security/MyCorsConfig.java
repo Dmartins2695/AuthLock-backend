b3a363f1faf5bf4c8163a0d149bc5ede7f9f12c9
Dmartins2695 <62670499+Dmartins2695@users.noreply.github.com>
2022-10-25 23:52:33 +0100

    refactor: cors policy headers

--- src/main/java/com/webapp/pwmanager/security/MyCorsConfig.java
+++ src/main/java/com/webapp/pwmanager/security/MyCorsConfig.java
@@ -17,7 +17,7 @@ public class MyCorsConfig implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         final HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "https://authlock.netlify.app/");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, enctype");
         response.setHeader("Access-Control-Max-Age", "3600");
