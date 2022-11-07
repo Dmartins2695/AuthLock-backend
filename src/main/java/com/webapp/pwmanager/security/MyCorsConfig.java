b99dd2d83d836e53d722c699e6ecef880a405b12
Dmartins2695 <62670499+Dmartins2695@users.noreply.github.com>
2022-11-07 00:00:11 +0000

    fix: try 1

--- src/main/java/com/webapp/pwmanager/security/MyCorsConfig.java
+++ src/main/java/com/webapp/pwmanager/security/MyCorsConfig.java
@@ -22,7 +22,7 @@ public class MyCorsConfig implements Filter{
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+        response.setHeader("Access-Control-Allow-Headers", "*");
         if (HttpMethod.OPTIONS.name().equalsIgnoreCase(((HttpServletRequest) servletRequest).getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
